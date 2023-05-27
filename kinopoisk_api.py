5a311068a5431a2269ac3ee983d6b22d5c91029b
yxandrw <cher18@yandex.ru>
2023-05-27 23:45:15 +0400

    Update kinopoisk_api.py

--- kinopoisk_api.py
+++ kinopoisk_api.py
@@ -13,23 +13,30 @@ text_token = '&token=XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX'
 text_year = '&year='
 linktokino = 'https://www.kinopoisk.ru/film/'
 
-df = pd.DataFrame(columns=['url', 'title', 'rating','year'])
-for film_number in range(0,446):
-    film = films.loc[film_number]
-    film_year = str(film.year)
-    encoded_string = urllib.parse.quote(film.title)
-    text_full = text_begin+encoded_string+text_token+text_year+film_year
-    response = requests.get(text_full)
-    items = json.loads(response.text)
-    if items['pages'] ==0:
-        df_err = pd.DataFrame([[0,film.title,0,0]],columns=['url', 'title', 'rating','year'])
-        df = pd.concat([df, df_err], ignore_index=True)
-    else:
-        item_id = str(items['docs'][0]['id'])
-        item_name = items['docs'][0]['name']
-        item_rating = items['docs'][0]['rating']['kp']
-        item_year = items['docs'][0]['year']
-        export = linktokino+item_id, item_name, item_rating, item_year
-        df_true = pd.DataFrame([export], columns=['url', 'title', 'rating','year'])
-        df = pd.concat([df, df_true], ignore_index=True)
-df.to_excel("output.xlsx")
+def fetch_film_data(films_data):
+    df = pd.DataFrame(columns=['url', 'title', 'rating', 'year'])
+    for film_number in range(len(films_data)):
+        film = str(films_data.loc[film_number].title)
+        film_year = str(films_data.loc[film_number].year)
+        encoded_string = urllib.parse.quote(film) 
+        text_full = text_begin + encoded_string + text_token + text_year + film_year
+        response = requests.get(text_full)
+        items = json.loads(response.text)
+
+        if items['pages'] == 0:
+            df_err = pd.DataFrame([[0, films_data.loc[film_number].title, 0, 0]], columns=['url', 'title', 'rating', 'year'])
+            df = pd.concat([df, df_err], ignore_index=True)
+        else:
+            item_id = str(items['docs'][0]['id'])
+            item_name = items['docs'][0]['name']
+            item_rating = items['docs'][0]['rating']['kp']
+            item_year = items['docs'][0]['year']
+            export = linktokino + item_id, item_name, item_rating, item_year
+            df_true = pd.DataFrame([export], columns=['url', 'title', 'rating', 'year'])
+            df = pd.concat([df, df_true], ignore_index=True)
+
+    return df
+
+df_films = fetch_film_data(films)
+
+df_films.to_excel("output.xlsx")
