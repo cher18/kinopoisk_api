5fb9e2e6785bb39118192a05ddddc7f204b5b74a
yxandrw <cher18@yandex.ru>
2023-06-04 11:09:56 +0400

    refactoring

--- kinopoisk_api.py
+++ kinopoisk_api.py
@@ -15,16 +15,14 @@ linktokino = 'https://www.kinopoisk.ru/film/'
 
 def fetch_film_data(films_data):
     df = pd.DataFrame(columns=['url', 'title', 'rating', 'year'])
-    for film_number in range(len(films_data)):
-        film = str(films_data.loc[film_number].title)
-        film_year = str(films_data.loc[film_number].year)
-        encoded_string = urllib.parse.quote(film) 
-        text_full = text_begin + encoded_string + text_token + text_year + film_year
+    for row in films.itertuples(index=False):
+        encoded_string = urllib.parse.quote(str(row.title))
+        text_full = text_begin + encoded_string + text_token + text_year + str(row.year)
         response = requests.get(text_full)
         items = json.loads(response.text)
         # нужно сделать проверку по году +-1/2 eyear, ибо многое фильмы различаются
         if items['pages'] == 0:
-            df_err = pd.DataFrame([[0, films_data.loc[film_number].title, 0, 0]], columns=['url', 'title', 'rating', 'year'])
+            df_err = pd.DataFrame([[0, row.title, 0, 0]], columns=['url', 'title', 'rating', 'year'])
             df = pd.concat([df, df_err], ignore_index=True)
         else:
             item_id = str(items['docs'][0]['id'])
